8e7282c426805e48f6e52a4f592c5e7d16b11c8d
anningaaaa <86214194+anningaaaa@users.noreply.github.com>
2021-06-22 18:12:58 +0800

    Add files via upload

---
+++ techan.py
@@ -0,0 +1,81 @@
+# 驴肉火烧
+id1 = "驴肉火烧"
+price1 = 11
+origin1 = "保定"
+taste = "非常好"
+# 露露
+id2 = "露露"
+price2 = 20
+origin2 = "承德"
+taste = "非常好"
+# 焖子
+id3 = "焖子"
+price3 = 10
+origin3 = "定州"
+taste = "非常好"
+# 大枣
+id4 = "大枣"
+price4 = 15
+origin4 = "顺平"
+taste = "非常好"
+# 苹果
+id5 = "苹果"
+price5 = 30
+origin5 = "邢台"
+taste = "非常好"
+# 莜面
+id6 = "莜面"
+price6 = 15
+origin6 = "张家口"
+taste = "非常好"
+# 马头酥鱼
+id7 = "马头酥鱼"
+price7 = 30
+origin7 = "邯郸"
+taste = "非常好"
+# 马家卤鸡
+id8 = "马家卤鸡"
+price8 = 25
+origin8 = "石家庄"
+taste = "非常好"
+# 石门核桃
+id9 = "石门核桃"
+price9 = 10
+origin9 = "秦皇岛"
+taste = "非常好"
+# 蜜蜂麻糖
+id10 = "蜜蜂麻糖"
+price10 = 20
+origin10 = "唐山"
+taste = "非常好"
+# 焦通香肠
+id11 = "焦通香肠"
+price11 = 25
+origin11 = "廊坊"
+taste = "非常好"
+# 火锅鸡
+id12 = "火锅鸡"
+price12 = 58
+origin12 = "沧州"
+taste = "非常好"
+# 老白干
+id13 = "老白干"
+price13 = 45
+origin13 = "衡水"
+taste = "非常好"
+
+print("----------河北美食----------")
+print("--美食\t\t价格\t\t\t产地\t\t\t品质--")
+print(id1,"\t\t",price1,"\t\t",origin1,"\t\t",taste)
+print(id2,"\t\t",price2,"\t\t",origin2,"\t\t",taste)
+print(id3,"\t\t",price3,"\t\t",origin3,"\t\t",taste)
+print(id4,"\t\t",price4,"\t\t",origin4,"\t\t",taste)
+print(id5,"\t\t",price5,"\t\t",origin5,"\t\t",taste)
+print(id6,"\t\t",price6,"\t\t",origin6,"\t\t",taste)
+print(id7,"\t\t",price7,"\t\t",origin7,"\t\t",taste)
+print(id8,"\t\t",price8,"\t\t",origin8,"\t\t",taste)
+print(id9,"\t\t",price9,"\t\t",origin9,"\t\t",taste)
+print(id10,"\t\t",price10,"\t\t",origin10,"\t\t",taste)
+print(id11,"\t\t",price11,"\t\t",origin11,"\t\t",taste)
+print(id12,"\t\t",price12,"\t\t",origin12,"\t\t",taste)
+print(id13,"\t\t",price13,"\t\t",origin13,"\t\t",taste)
