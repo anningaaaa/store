7df981a6cd251d0be9c16689e18464467a6bd97a
anningaaaa <86214194+anningaaaa@users.noreply.github.com>
2021-06-23 17:39:38 +0800

    Add files via upload

---
+++ suijishu.py
@@ -0,0 +1,20 @@
+import random
+
+a = random.randint(0,200)
+i = 0
+c = 5000
+while i <= 20:
+    if c < 500:
+        break
+    b = input("请输入您要猜的数字：")
+    b = int(b)
+    if b > a:
+        print("大了！")
+        c = c - 500
+    elif b < a:
+        print("小了！")
+        c = c - 50050
+    else:
+        print("恭喜您，猜中数字，本次幸运数字为：",b,"您一共猜了：",i+1,"次。","您还有金币：",c+1000)
+        break
+    i = i + 1
